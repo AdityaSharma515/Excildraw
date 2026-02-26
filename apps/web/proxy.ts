4e4d7c7fde82490b3e4ca8031c9eb8b44bbaf2fa
Aditya Sharma <adsharma515@gmail.com>
2026-02-27 03:01:07 +0530

    feat:Added route protection middleware in fe

---
+++ apps/web/proxy.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req:NextRequest){
+    const token=req.cookies.get("token")?.value
+    if(!token){
+        return NextResponse.redirect(new URL("/",req.url))
+    }
+
+    try {
+        const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL
+        const res = await fetch(`${BASE_URL}/auth/me`, {
+          headers: {
+            cookie: `token=${token}`,
+          },
+          cache: "no-store",
+        })
+        if(!res.ok){
+            return NextResponse.redirect(new URL("/",req.url))
+        }
+        return NextResponse.next();
+    } catch (error) {
+        return NextResponse.redirect(new URL("/",req.url))
+    }
+}
+
+export const config={
+    matcher:["/dashboard/:path*","/canvas/:path*"]
+}
