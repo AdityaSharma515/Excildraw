dc2c59528df49aea1329908cb274887b0e22184a
Aditya Sharma <adsharma515@gmail.com>
2026-02-28 00:17:41 +0530

    feat:added docker compose file

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,12 +1,13 @@
+import path from "path";
+import dotenv from "dotenv"
+dotenv.config({path: path.resolve(__dirname, "../.env")});
 import express from "express";
 import router from "./routes/index";
 import cors from "cors"
-import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import passport from "passport";
 import "./config/passport-setup"
 const app=express();
-dotenv.config();
 app.use(express.json());
 app.use(cors({
     origin: process.env.FRONTEND_URL || "http://localhost:3000",
