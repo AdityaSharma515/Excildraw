673712915951363b9bd61e6787b74be1e034cf3c
Aditya Sharma <adsharma515@gmail.com>
2026-03-01 00:06:43 +0530

    fix: added down script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 cd Excildraw
 git pull origin main
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml pull
 docker compose -f docker-compose.prod.yml up -d --build
